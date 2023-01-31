ed6963e0b9567ffcbf8e5edc43d4e480aaadebf6
kemcet <kemalcetmensm@gmail.com>
2023-01-31 19:47:55 +0300

    DESIGN end header

--- src/components/layouts/Main/Header/index.tsx
+++ src/components/layouts/Main/Header/index.tsx
@@ -1,28 +1,45 @@
+import Image from 'next/image'
 import React from 'react'
 import styles from './index.module.css'
+import { TbSearch } from 'react-icons/tb';
+
+import { IconContext } from "react-icons";
+import { HiOutlineShoppingCart } from 'react-icons/hi';
+import { AiOutlineHeart } from 'react-icons/ai';
+import { MdPersonOutline } from 'react-icons/md';
 
 const index = () => {
     return (
         <header className={styles.container}>
             <div className={styles.logo}>
-                sddsf
-                sdv
+                <Image
+                    src="./logo.svg"
+                    alt="logo"
+                    width="60"
+                    height="39"
+                />
             </div>
             <div className={styles.search}>
-                <input/>
+                <TbSearch color="#9ca3af" />
+                <input
+                    placeholder="Search"
+                // value={search}
+                // onChange={handleChange}
+                />
             </div>
-            <nav className={styles.nav}>
-                <div className={styles.navitem}>
-                    a
-                </div>
-                <div className={styles.navitem}>
-                    b
-                </div>
-                <div className={styles.navitem}>
-                    c
-                </div>
-
-            </nav>
+            <IconContext.Provider value={{ color: "#090937", size: "25" }}>
+                <nav className={styles.nav}>
+                    <div className={styles.navitem}>
+                        <MdPersonOutline />
+                    </div>
+                    <div className={styles.navitem}>
+                        <AiOutlineHeart />
+                    </div>
+                    <div className={styles.navitem}>
+                        <HiOutlineShoppingCart />
+                    </div>
+                </nav>
+            </IconContext.Provider>
         </header>
     )
 }
